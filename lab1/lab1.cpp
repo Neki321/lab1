dd173a7bc59bf6791841eb910fa7ae33a8acd78c
Neki321 <vlad.furman.13@gmail.com>
2022-12-13 17:30:28 +0200

    Initial commit

---
+++ lab1/lab1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+    string a;
+    cout << "Hello, what is your name?" << endl;
+    cin >> a;
+    cout << "Nice to meet you, " << a << " <3";
+}
